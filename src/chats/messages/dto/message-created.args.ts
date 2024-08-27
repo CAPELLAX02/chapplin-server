eb433c8177f3256847d68049a6f19b6443d1743b
CAPELLAX02 <ahmtatar@gmail.com>
2024-08-27 15:34:20 +0300

    Multiple Message Subsriptions - Backend

--- src/chats/messages/dto/message-created.args.ts
+++ src/chats/messages/dto/message-created.args.ts
@@ -1,13 +1,10 @@
 import { ArgsType, Field } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
+import { IsArray, IsNotEmpty } from 'class-validator';
 
 @ArgsType()
 export class MessageCreatedArgs {
-  /**
-   * The ID of the chat for which messages are being subscribed.
-   * This field is required.
-   */
-  @Field()
-  @IsNotEmpty()
-  chatId: string;
+  @Field(() => [String])
+  @IsArray()
+  @IsNotEmpty({ each: true })
+  chatIds: string[];
 }
