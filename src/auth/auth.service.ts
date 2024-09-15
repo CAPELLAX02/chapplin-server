67e2156faa74d289002067dd164e4f6129906511
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-16 01:22:07 +0300

    code cleanup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,13 +12,6 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  /**
-   * Logs in a user by generating a JWT and setting it as a cookie in the response.
-   *
-   * @param {User} user - The user entity to log in.
-   * @param {Response} response - The HTTP response object to set the cookie on.
-   * @returns {Promise<void>} - Resolves when the login process is complete.
-   */
   async login(user: User, response: Response) {
     const expires = new Date();
     expires.setSeconds(
@@ -38,12 +31,6 @@ export class AuthService {
     });
   }
 
-  /**
-   * Logs out a user by clearing the 'Authentication' cookie in the response.
-   *
-   * @param {Response} response - The HTTP response object to clear the cookie from.
-   * @returns {void}
-   */
   logout(response: Response) {
     response.cookie('Authentication', '', {
       httpOnly: true,
@@ -51,16 +38,6 @@ export class AuthService {
     });
   }
 
-  /**
-   * Verifies the WebSocket connection request by extracting the JWT from cookies.
-   *
-   * Extracts the JWT token from the 'Authentication' cookie in the incoming request
-   * and verifies its validity. If valid, returns the decoded token payload.
-   *
-   * @param {Request} request - The incoming request object from the WebSocket context.
-   * @returns {TokenPayload} - The verified JWT payload containing user information.
-   * @throws {UnauthorizedException} - Throws if the JWT is invalid or missing.
-   */
   verifyWebSocket(request: Request): TokenPayload {
     const cookies: string[] = request.headers.cookie.split('; ');
     const authCookie = cookies.find((cookie) =>
@@ -71,7 +48,7 @@ export class AuthService {
       throw new UnauthorizedException('Authentication token missing');
     }
 
-    const jwt = authCookie.split('=')[1].trim(); // '=' işaretinden sonraki kısmı al ve boşlukları temizle
+    const jwt = authCookie.split('=')[1].trim();
 
     try {
       return this.jwtService.verify(jwt);
